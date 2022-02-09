676a63f7c6838b7e0655e755d1d064a56b89e012
Ivan Volkov <volkov.ivan2004@gmail.com>
2022-02-09 04:40:52 +0300

    raw version of task done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
-fun main() {
+import java.io.*
+import kotlin.random.*
+import kotlin.system.measureTimeMillis
 
+fun testIO(nTimes: Int, rng: Random = Random(239), size: Int = 128 * 1024 * 1024) {
+    var sm_write: Long = 0
+    var sm_read: Long = 0
+    repeat(nTimes) {
+        val out = FileOutputStream("tmp_file")
+        val write_data = rng.nextBytes(size)
+        val cur_write = measureTimeMillis {
+            out.write(write_data)
+            out.channel.force(true)
+        }
+        out.close()
+
+        val inp = FileInputStream("tmp_file")
+        val read_data: ByteArray
+        val cur_read = measureTimeMillis {
+            read_data = inp.readBytes()
+        }
+        assert(read_data.contentEquals(write_data))
+        inp.close()
+
+        sm_write += cur_write
+        sm_read += cur_read
+        println("Write = $cur_write, read = $cur_read")
+    }
+    println("Avg: Write = ${sm_write / nTimes}, Read = ${sm_read / nTimes}")
+}
+
+fun main() {
+    testIO(10)
 }

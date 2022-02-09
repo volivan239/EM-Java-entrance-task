adf5f18f5e25041ebd822416c2acd91d40cf1cf3
Ivan Volkov <volkov.ivan2004@gmail.com>
2022-02-10 02:40:15 +0300

    Code organization improvement & added results

---
+++ src/main/kotlin/IOSpeedTester.kt
@@ -0,0 +1,41 @@
+import java.io.*
+import kotlin.random.Random
+import kotlin.io.path.createTempFile
+import kotlin.math.roundToInt
+import kotlin.system.measureTimeMillis
+
+/**
+ * @param file File to which test data will be written
+ * @param writeSize Size of data (in MB) that will be written/read in each test
+ */
+class IOSpeedTester(
+    private val file: File = createTempFile().toFile(),
+    private val writeSize: Int = 256
+) {
+    private val rng: Random = Random
+
+    /**
+     * nTimes times writes and reads random data to/from disk, measuring speed
+     * @return results for each attempt: first element is writing speed (in MB/s), second is reading speed (in MB/s)
+     */
+    fun doTests(nTimes: Int): List<Pair<Int, Int>> = List(nTimes) {
+        val writeData = rng.nextBytes(writeSize * 1024 * 1024)
+
+        val out = FileOutputStream(file)
+        val curWrite = 0.001 * measureTimeMillis {
+            out.write(writeData)
+            out.channel.force(true)
+        }
+        out.close()
+
+        val inp = FileInputStream(file)
+        val readData: ByteArray
+        val curRead = 0.001 * measureTimeMillis {
+            readData = inp.readBytes()
+        }
+        assert(readData.contentEquals(writeData))
+        inp.close()
+
+        Pair((writeSize / curWrite).roundToInt(), (writeSize / curRead).roundToInt())
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,9 @@
-import java.io.*
-import kotlin.random.*
-import kotlin.system.measureTimeMillis
-
-fun testIO(nTimes: Int, rng: Random = Random(239), size: Int = 128 * 1024 * 1024) {
-    var sm_write: Long = 0
-    var sm_read: Long = 0
-    repeat(nTimes) {
-        val out = FileOutputStream("tmp_file")
-        val write_data = rng.nextBytes(size)
-        val cur_write = measureTimeMillis {
-            out.write(write_data)
-            out.channel.force(true)
-        }
-        out.close()
-
-        val inp = FileInputStream("tmp_file")
-        val read_data: ByteArray
-        val cur_read = measureTimeMillis {
-            read_data = inp.readBytes()
-        }
-        assert(read_data.contentEquals(write_data))
-        inp.close()
-
-        sm_write += cur_write
-        sm_read += cur_read
-        println("Write = $cur_write, read = $cur_read")
+fun main(args: Array<String>) {
+    val nTimes = args[0].toInt()
+    val results = IOSpeedTester().doTests(nTimes)
+    results.forEachIndexed { ind, value ->
+        println("Test #${ind + 1}: Writing speed = ${value.first} MB/s, Reading speed = ${value.second} MB/s")
     }
-    println("Avg: Write = ${sm_write / nTimes}, Read = ${sm_read / nTimes}")
-}
-
-fun main() {
-    testIO(10)
+    println("\nAvg Writing speed = ${results.sumOf { it.first } / nTimes} MB/s," +
+               " Reading speed = ${results.sumOf { it.second } / nTimes} MB/s")
 }
